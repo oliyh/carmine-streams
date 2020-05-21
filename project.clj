00f3707805ca929543bf8fa682e589c8a9f9a068
Oliver Hine <git@oliy.co.uk>
2020-05-21 14:58:12 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject carmine-streams "0.1.1-SNAPSHOT"
+(defproject carmine-streams "0.1.1"
   :description "Carmine helpers for redis streams"
   :url "http://github.com/oliyh/carmine-streams"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
