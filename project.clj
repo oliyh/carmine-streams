9995adbaebb255f9c8f654f511839204af8f3853
Oliver Hine <git@oliy.co.uk>
2022-07-11 10:25:33 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/oliyh/carmine-streams"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.taoensso/carmine "2.20.0-RC1"]
-                 [org.clojure/tools.logging "1.1.0"]]
+  :dependencies [[com.taoensso/carmine "3.1.0"]
+                 [org.clojure/tools.logging "1.2.4"]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}}
   :repl-options {:init-ns carmine-streams.core})
